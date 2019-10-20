9ec0d810b72461ab7cd95f682a439e26377c9959
choegunho <yn962271@gmail.com>
2019-10-20 22:03:22 +0900

    modified

--- practice.c
+++ practice.c
@@ -3,6 +3,13 @@
 int main(void){
 	
 	printf("practice\n");
-
+	printf("practice merged\n");
+  	
+	for (int i = 2; i < 10; i++){
+		for (int j = 1; j < 10; j++){
+			printf("%d X %d = %d\n", i, j, i * j);
+		}
+		printf("\n");
+	}
 	return 0;
 }
